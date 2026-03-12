dc4c836aa19e64a5c5e5c64609d3e43288d0d7c8
1SamXD <johnson.samuel2005@gmail.com>
2026-03-12 18:29:24 -0400

    now it has micro ros and cmd_vel

--- motor_test/motor_test.ino
+++ motor_test/motor_test.ino
@@ -1,45 +1,94 @@
-// Sabertooth PWM for Teensy without external libraries
+#include <micro_ros_arduino.h>
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <geometry_msgs/msg/twist.h>
+
+// --- Hardware Pins ---
 const int motor1Pin = 0; 
 const int motor2Pin = 1;
 
-float angle = 0.0;
-const float increment = 0.01; 
+// --- PWM Constants (12-bit) ---
+const int neutral = 307;   // 1500us
+const int range = 100;     // Max deviation from neutral (approx 500us)
 
-// For 12-bit resolution (0 to 4095)
-// 1500us (Neutral) is approx 307
-// 1000us (Reverse) is approx 205
-// 2000us (Forward) is approx 410
-const int neutral = 307;
-const int amplitude = 100; 
+// --- micro-ROS Objects ---
+rcl_subscription_t subscriber;
+geometry_msgs__msg__Twist msg;
+rclc_executor_t executor;
+rcl_node_t node;
+rcl_allocator_t allocator;
+rclc_support_t support;
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
+void error_loop(){
+  while(1){
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    delay(100);
+  }
+}
+
+// --- Callback: This runs whenever Foxglove sends a cmd_vel message ---
+void subscription_callback(const void * msgin) {
+  const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msgin;
+
+  // Linear x: Forward/Reverse (-1.0 to 1.0)
+  // Angular z: Left/Right (-1.0 to 1.0)
+  float linear = msg->linear.x;
+  float angular = msg->angular.z;
+
+  // Differential Drive Mixing
+  float left_val = linear - angular;
+  float right_val = linear + angular;
+
+  // Map to PWM values (12-bit)
+  int motor1Output = neutral + (left_val * range);
+  int motor2Output = neutral + (right_val * range);
+
+  // Constrain to prevent over-driving
+  motor1Output = constrain(motor1Output, neutral - range, neutral + range);
+  motor2Output = constrain(motor2Output, neutral - range, neutral + range);
+
+  analogWrite(motor1Pin, motor1Output);
+  analogWrite(motor2Pin, motor2Output);
+}
 
 void setup() {
+  set_microros_transports();
+  
   pinMode(motor1Pin, OUTPUT);
   pinMode(motor2Pin, OUTPUT);
-
-  // Set PWM frequency to 50Hz (Standard for RC/Sabertooth)
   analogWriteFrequency(motor1Pin, 50);
   analogWriteFrequency(motor2Pin, 50);
-
-  // Set resolution to 12-bit for smoother motion
   analogWriteResolution(12);
 
-  // Start at Neutral (Zero)
+  // Initialize to Neutral
   analogWrite(motor1Pin, neutral);
   analogWrite(motor2Pin, neutral);
-  delay(2000); 
+
+  delay(2000);
+
+  allocator = rcl_get_default_allocator();
+
+  // Initialize micro-ROS support
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+  RCCHECK(rclc_node_init_default(&node, "teensy_rover_node", "", &support));
+
+  // Initialize subscriber
+  RCCHECK(rclc_subscription_init_default(
+    &subscriber,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+    "cmd_vel"));
+
+  // Initialize executor
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
 }
 
 void loop() {
-  float sineValue = sin(angle);
-  
-  // Calculate output
-  int motorOutput = neutral + (sineValue * amplitude);
-  
-  analogWrite(motor1Pin, motorOutput);
-  analogWrite(motor2Pin, motorOutput);
-  
-  angle += increment;
-  if (angle > 2 * PI) angle -= 2 * PI;
-  
-  delay(20); 
+  // Check for new messages
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)));
 }
