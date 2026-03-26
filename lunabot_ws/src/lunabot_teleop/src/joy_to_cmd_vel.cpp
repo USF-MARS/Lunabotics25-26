f2f370f9db07b9177dd5147da2ecf929b51a6a7f
1SamXD <johnson.samuel2005@gmail.com>
2026-03-26 19:31:11 -0400

    both linear and motors

--- lunabot_ws/src/lunabot_teleop/src/joy_to_cmd_vel.cpp
+++ lunabot_ws/src/lunabot_teleop/src/joy_to_cmd_vel.cpp
@@ -1,39 +1,44 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "std_msgs/msg/int32.hpp" // Added for the actuator
 
 class JoyToCmdVel : public rclcpp::Node {
 public:
     JoyToCmdVel() : Node("joy_to_cmd_vel") {
-        // Subscribe to /joy (from Foxglove)
         subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
             "/joy", 10, std::bind(&JoyToCmdVel::joy_callback, this, std::placeholders::_1));
 
-        // Publish to /cmd_vel (to Teensy)
         publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+        // New publisher for the lift
+        lift_publisher_ = this->create_publisher<std_msgs::msg::Int32>("/lift_actuator", 10);
     }
 
 private:
     void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg) const {
+        // 1. Handle Drive (Twist)
         auto twist = geometry_msgs::msg::Twist();
-
-        // Xbox Left Stick Up/Down is usually index 1
-        // We multiply by a scale factor (e.g., 1.0)
         twist.linear.x = msg->axes[1] * 1.0; 
-        
-        // Right Stick Left/Right is usually index 3 (for future turning)
-        twist.angular.z = msg->axes[3] * 1.0;
-
+        twist.angular.z = msg->axes[3] * -1.0; // Inverting Z is common for natural steering
         publisher_->publish(twist);
+
+        // 2. Handle Lift Logic
+        auto lift_msg = std_msgs::msg::Int32();
+        
+        if (msg->buttons[5] == 1) {
+            lift_msg.data = 1;  // Extend
+        } 
+        else if (msg->buttons[4] == 1) {
+            lift_msg.data = -1; // Retract
+        } 
+        else {
+            lift_msg.data = 0;  // Stop
+        }
+        
+        lift_publisher_->publish(lift_msg);
     }
 
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr lift_publisher_;
 };
-
-int main(int argc, char * argv[]) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<JoyToCmdVel>());
-    rclcpp::shutdown();
-    return 0;
-}
--- lunabot_ws/src/lunabot_test_teensy/src/Lin_actuator_test/Lin_actuator_test.ino
+++ lunabot_ws/src/lunabot_test_teensy/src/Lin_actuator_test/Lin_actuator_test.ino
@@ -1,60 +1,158 @@
-// Pin Definitions for Teensy 4.1
-const int PWM_PIN = 19;  // PWM pin
-const int DIR_PIN = 17;  // Direction pin
+#include <micro_ros_arduino.h>
+#include <stdio.h>
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <geometry_msgs/msg/twist.h>
+#include <std_msgs/msg/int32.h>
+#include <ctype.h>
 
-// PWM Settings
-const int pwmFreq = 5000;    // 5 kHz frequency
-const int pwmResolution = 8; // 8-bit resolution (0-255)
+// --- 1. Locale Fix for Teensy/micro-ROS Toolchain ---
+#ifdef __locale_ctype_ptr
+#undef __locale_ctype_ptr
+#endif
+
+extern "C" {
+  extern const char _ctype_[];
+  const unsigned short* __locale_ctype_ptr(void) {
+    return (const unsigned short*)_ctype_;
+  }
+}
+
+// --- 2. Pin Definitions ---
+// Drive Motors (H-Bridges)
+const int motorLeft1 = 0; 
+const int motorLeft2 = 1;
+const int motorRight1 = 7; 
+const int motorRight2 = 8;
+
+// Linear Actuator
+const int LIFT_PWM_PIN = 19; 
+const int LIFT_DIR_PIN = 17;
+
+// --- 3. Constants ---
+const int neutral = 307;   // 1500us for 50Hz/12-bit
+const int range = 100;     // Speed offset
+const int liftFreq = 5000; // 5kHz for Actuator
+const int liftRes = 8;     // 8-bit (0-255) for Actuator
+
+// --- 4. micro-ROS Objects ---
+rcl_subscription_t drive_subscriber;
+rcl_subscription_t lift_subscriber;
+geometry_msgs__msg__Twist drive_msg;
+std_msgs__msg__Int32 lift_msg;
+rclc_executor_t executor;
+rcl_node_t node;
+rclc_support_t support;
+rcl_allocator_t allocator;
+
+// Error handling macros
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
+void error_loop(){
+  while(1){
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    delay(100);
+  }
+}
+
+// --- 5. Callback: Drive Motors (/cmd_vel) ---
+void drive_callback(const void * msvin) {
+  const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msvin;
+  
+  float linear_x = msg->linear.x;   
+  float angular_z = msg->angular.z; 
+
+  // Mix Linear and Angular for Differential Drive
+  float left_val  = linear_x - angular_z;
+  float right_val = linear_x + angular_z;
+
+  left_val  = constrain(left_val, -1.0, 1.0);
+  right_val = constrain(right_val, -1.0, 1.0);
+
+  int leftPWM  = neutral + (int)(left_val * range);
+  int rightPWM = neutral + (int)(right_val * range);
+  
+  analogWrite(motorLeft1, leftPWM);
+  analogWrite(motorLeft2, leftPWM);
+  analogWrite(motorRight1, rightPWM);
+  analogWrite(motorRight2, rightPWM);
+}
+
+// --- 6. Callback: Linear Actuator (/lift_actuator) ---
+void lift_callback(const void * msvin) {
+  const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msvin;
+  int command = msg->data;
+
+  if (command == 1) {        // Extend
+    digitalWrite(LIFT_DIR_PIN, HIGH);
+    analogWrite(LIFT_PWM_PIN, 255);
+  } 
+  else if (command == -1) {  // Retract
+    digitalWrite(LIFT_DIR_PIN, LOW);
+    analogWrite(LIFT_PWM_PIN, 255);
+  } 
+  else {                     // Stop
+    analogWrite(LIFT_PWM_PIN, 0);
+  }
+}
 
 void setup() {
-  Serial.begin(115200);
+  set_microros_transports();
   
-  // Configure Pins
-  pinMode(PWM_PIN, OUTPUT);
-  pinMode(DIR_PIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   
-  // Set PWM Frequency and Resolution for Teensy
-  analogWriteFrequency(PWM_PIN, pwmFreq);
-  analogWriteResolution(pwmResolution);
+  // Setup Drive Pins
+  pinMode(motorLeft1, OUTPUT);
+  pinMode(motorLeft2, OUTPUT);
+  pinMode(motorRight1, OUTPUT);
+  pinMode(motorRight2, OUTPUT);
+  analogWriteFrequency(motorLeft1, 50);
+  analogWriteFrequency(motorRight1, 50); // Setting freq for one pin in group is usually enough
+  analogWriteResolution(12);
   
-  Serial.println("Teensy 4.1 Actuator Emulator Ready.");
-  Serial.println("Commands: 'e' (Extend), 'r' (Retract), 's' (Stop)");
+  // Setup Actuator Pins
+  pinMode(LIFT_PWM_PIN, OUTPUT);
+  pinMode(LIFT_DIR_PIN, OUTPUT);
+  analogWriteFrequency(LIFT_PWM_PIN, liftFreq);
+  // Note: analogWriteResolution is global; we'll handle the 8-bit scale manually or set to 12.
+  // To keep it simple, we use the 12-bit resolution (0-4095) for everything.
+  
+  // Initialize to Neutral/Stop
+  analogWrite(motorLeft1, neutral);
+  analogWrite(motorLeft2, neutral);
+  analogWrite(motorRight1, neutral);
+  analogWrite(motorRight2, neutral);
+  analogWrite(LIFT_PWM_PIN, 0);
+
+  delay(2000);
+
+  allocator = rcl_get_default_allocator();
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+  RCCHECK(rclc_node_init_default(&node, "teensy_robot_node", "", &support));
+
+  // Init Drive Sub
+  RCCHECK(rclc_subscription_init_default(
+    &drive_subscriber,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+    "cmd_vel"));
+
+  // Init Lift Sub
+  RCCHECK(rclc_subscription_init_default(
+    &lift_subscriber,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "lift_actuator"));
+
+  // Create Executor for 2 handles
+  RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+  RCCHECK(rclc_executor_add_subscription(&executor, &drive_subscriber, &drive_msg, &drive_callback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_subscription(&executor, &lift_subscriber, &lift_msg, &lift_callback, ON_NEW_DATA));
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    char cmd = Serial.read();
-    
-    // Clean up newline characters
-    if (cmd == '\n' || cmd == '\r') return;
-
-    if (cmd == 'e') {
-      moveActuator(1.0); 
-      Serial.println(">> Action: EXTENDING");
-    } 
-    else if (cmd == 'r') {
-      moveActuator(-1.0);
-      Serial.println(">> Action: RETRACTING");
-    } 
-    else if (cmd == 's') {
-      moveActuator(0.0);
-      Serial.println(">> Action: STOPPED");
-    }
-  }
-}
-
-void moveActuator(float command) {
-  // 1. Set Direction
-  if (command > 0) {
-    digitalWrite(DIR_PIN, HIGH);
-  } else {
-    digitalWrite(DIR_PIN, LOW);
-  }
-
-  // 2. Calculate PWM (0 to 255 for 8-bit)
-  int dutyCycle = abs(command) * 255;
-  dutyCycle = constrain(dutyCycle, 0, 255);
-  
-  // 3. Standard Arduino/Teensy PWM command
-  analogWrite(PWM_PIN, dutyCycle);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)));
 }
