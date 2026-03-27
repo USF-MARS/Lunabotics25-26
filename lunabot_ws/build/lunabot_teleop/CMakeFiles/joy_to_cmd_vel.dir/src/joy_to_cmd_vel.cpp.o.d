c84f464ec92eb828166bd83584a1baa8682b8b54
mars-usf <mars-usf>
2026-03-26 20:16:23 -0400

    rggrs

--- lunabot_ws/build/lunabot_teleop/CMakeFiles/joy_to_cmd_vel.dir/src/joy_to_cmd_vel.cpp.o.d
+++ lunabot_ws/build/lunabot_teleop/CMakeFiles/joy_to_cmd_vel.dir/src/joy_to_cmd_vel.cpp.o.d
@@ -700,4 +700,10 @@ CMakeFiles/joy_to_cmd_vel.dir/src/joy_to_cmd_vel.cpp.o: \
  /opt/ros/jazzy/include/geometry_msgs/geometry_msgs/msg/detail/twist__traits.hpp \
  /opt/ros/jazzy/include/geometry_msgs/geometry_msgs/msg/detail/vector3__traits.hpp \
  /opt/ros/jazzy/include/geometry_msgs/geometry_msgs/msg/detail/twist__type_support.hpp \
- /opt/ros/jazzy/include/geometry_msgs/geometry_msgs/msg/rosidl_generator_cpp__visibility_control.hpp
+ /opt/ros/jazzy/include/geometry_msgs/geometry_msgs/msg/rosidl_generator_cpp__visibility_control.hpp \
+ /opt/ros/jazzy/include/std_msgs/std_msgs/msg/int32.hpp \
+ /opt/ros/jazzy/include/std_msgs/std_msgs/msg/detail/int32__struct.hpp \
+ /opt/ros/jazzy/include/std_msgs/std_msgs/msg/detail/int32__builder.hpp \
+ /opt/ros/jazzy/include/std_msgs/std_msgs/msg/detail/int32__traits.hpp \
+ /opt/ros/jazzy/include/std_msgs/std_msgs/msg/detail/int32__type_support.hpp \
+ /opt/ros/jazzy/include/std_msgs/std_msgs/msg/rosidl_generator_cpp__visibility_control.hpp
--- lunabot_ws/src/lunabot_test_teensy/src/Lin_actuator_test/Lin_actuator_test.ino
+++ lunabot_ws/src/lunabot_test_teensy/src/Lin_actuator_test/Lin_actuator_test.ino
@@ -8,7 +8,10 @@
 #include <std_msgs/msg/int32.h>
 #include <ctype.h>
 
-// --- Teensy/micro-ROS Locale Fix ---
+// --- Hardware Toggles ---
+const bool invert_right_side = false; 
+
+// --- Locale Fix for Teensy/micro-ROS ---
 #ifdef __locale_ctype_ptr
 #undef __locale_ctype_ptr
 #endif
@@ -21,25 +24,27 @@ extern "C" {
 }
 
 // --- Hardware Pins ---
-const int motorLeft1  = 0; 
-const int motorLeft2  = 1;
+const int motorLeft1 = 0; 
+const int motorLeft2 = 1;
 const int motorRight1 = 7; 
 const int motorRight2 = 8;
 const int LIFT_PWM_PIN = 19;
 const int LIFT_DIR_PIN = 17;
 
-// --- PWM Constants (12-bit = 0 to 4095) ---
-const int driveNeutral = 307;  // 1500us
-const int driveRange   = 100; 
-const int liftFullPower = 4095; // 100% Duty Cycle at 12-bit
+// --- PWM Constants (12-bit Resolution) ---
+const int driveNeutral = 307;   // 1500us at 50Hz
+const int driveRange   = 100;   
+const int liftFullPower = 4095; // 100% duty cycle at 12-bit
 
 // --- micro-ROS Objects ---
 rclc_support_t support;
 rcl_node_t node;
 rcl_allocator_t allocator;
 rclc_executor_t executor;
+
 rcl_subscription_t drive_sub;
 geometry_msgs__msg__Twist drive_msg;
+
 rcl_subscription_t lift_sub;
 std_msgs__msg__Int32 lift_msg;
 
@@ -53,65 +58,85 @@ void error_loop(){
   }
 }
 
+// --- Drivetrain Callback ---
 void drive_callback(const void * msvin) {
-  const geometry_msgs__msg__Twist * incoming_msg = (const geometry_msgs__msg__Twist *)msvin;
-  float left_val  = constrain(incoming_msg->linear.x - incoming_msg->angular.z, -1.0, 1.0);
-  float right_val = constrain(incoming_msg->linear.x + incoming_msg->angular.z, -1.0, 1.0);
+  const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msvin;
+  
+  float left_val  = msg->linear.x - msg->angular.z;
+  float right_val = msg->linear.x + msg->angular.z;
 
-  analogWrite(motorLeft1, driveNeutral + (int)(left_val * driveRange));
-  analogWrite(motorLeft2, driveNeutral + (int)(left_val * driveRange));
-  analogWrite(motorRight1, driveNeutral + (int)(right_val * driveRange));
-  analogWrite(motorRight2, driveNeutral + (int)(right_val * driveRange));
+  // Normalization
+  float max_val = max(abs(left_val), abs(right_val));
+  if (max_val > 1.0) {
+    left_val /= max_val;
+    right_val /= max_val;
+  }
+
+  if (invert_right_side) right_val = -right_val;
+
+  int leftPWM  = driveNeutral + (int)(left_val * driveRange);
+  int rightPWM = driveNeutral + (int)(right_val * driveRange);
+  
+  analogWrite(motorLeft1, leftPWM);
+  analogWrite(motorLeft2, leftPWM);
+  analogWrite(motorRight1, rightPWM);
+  analogWrite(motorRight2, rightPWM);
 }
 
+// --- Lift Callback ---
 void lift_callback(const void * msvin) {
-  const std_msgs__msg__Int32 * incoming_msg = (const std_msgs__msg__Int32 *)msvin;
-  int command = incoming_msg->data;
+  const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msvin;
+  int command = msg->data;
 
-  if (command == 1) {        // EXTEND
+  if (command == 1) {
     digitalWrite(LIFT_DIR_PIN, HIGH);
-    analogWrite(LIFT_PWM_PIN, liftFullPower); 
-  } 
-  else if (command == -1) {  // RETRACT
+    analogWrite(LIFT_PWM_PIN, liftFullPower);
+  } else if (command == -1) {
     digitalWrite(LIFT_DIR_PIN, LOW);
     analogWrite(LIFT_PWM_PIN, liftFullPower);
-  } 
-  else {                     // STOP
+  } else {
     analogWrite(LIFT_PWM_PIN, 0);
   }
 }
 
 void setup() {
   set_microros_transports();
-  pinMode(LED_BUILTIN, OUTPUT);
   
-  // Drivetrain Setup
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(motorLeft1, OUTPUT); pinMode(motorLeft2, OUTPUT);
   pinMode(motorRight1, OUTPUT); pinMode(motorRight2, OUTPUT);
-  analogWriteFrequency(motorLeft1, 50); 
+  pinMode(LIFT_PWM_PIN, OUTPUT); pinMode(LIFT_DIR_PIN, OUTPUT);
+
+  // Frequency Setup
+  // NOTE: Pins 0, 1, 7, 8 are on different timers than Pin 19 on Teensy 4.1
+  analogWriteFrequency(motorLeft1, 50);
   analogWriteFrequency(motorRight1, 50);
-  
-  // Actuator Setup
-  pinMode(LIFT_PWM_PIN, OUTPUT);
-  pinMode(LIFT_DIR_PIN, OUTPUT);
   analogWriteFrequency(LIFT_PWM_PIN, 5000); 
-
-  // Global Resolution for Teensy
+  
   analogWriteResolution(12);
-
-  // Default States
+  
+  // Initial States
   analogWrite(motorLeft1, driveNeutral);
   analogWrite(motorLeft2, driveNeutral);
   analogWrite(motorRight1, driveNeutral);
   analogWrite(motorRight2, driveNeutral);
   analogWrite(LIFT_PWM_PIN, 0);
 
-  delay(2000);
+  // Wait for Agent Handshake (Prevents the crash/blink)
+  while (RMW_RET_OK != rmw_uros_ping_agent(100, 1)) {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    delay(500); 
+  }
+
   allocator = rcl_get_default_allocator();
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
   RCCHECK(rclc_node_init_default(&node, "teensy_robot_node", "", &support));
+
+  // Subscriptions
   RCCHECK(rclc_subscription_init_default(&drive_sub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist), "cmd_vel"));
   RCCHECK(rclc_subscription_init_default(&lift_sub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "lift_actuator"));
+
+  // Executor (Handle count MUST be 2)
   RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
   RCCHECK(rclc_executor_add_subscription(&executor, &drive_sub, &drive_msg, &drive_callback, ON_NEW_DATA));
   RCCHECK(rclc_executor_add_subscription(&executor, &lift_sub, &lift_msg, &lift_callback, ON_NEW_DATA));
