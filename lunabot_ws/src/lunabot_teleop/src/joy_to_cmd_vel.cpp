cb19032eac34ebcf21b7c995e895ab0bc0e4e893
1SamXD <johnson.samuel2005@gmail.com>
2026-04-29 12:00:19 -0400

    updated with ema and bucket and arm linear actuator

--- lunabot_ws/src/lunabot_teleop/src/joy_to_cmd_vel.cpp
+++ lunabot_ws/src/lunabot_teleop/src/joy_to_cmd_vel.cpp
@@ -1,7 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 #include "geometry_msgs/msg/twist.hpp"
-#include "std_msgs/msg/int32.hpp" // Added for the actuator
+#include "std_msgs/msg/int32.hpp"
 
 class JoyToCmdVel : public rclcpp::Node {
 public:
@@ -9,39 +9,63 @@ public:
         subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
             "/joy", 10, std::bind(&JoyToCmdVel::joy_callback, this, std::placeholders::_1));
 
-        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
-        // New publisher for the lift
-        lift_publisher_ = this->create_publisher<std_msgs::msg::Int32>("/lift_actuator", 10);
+        drive_publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+        arm_publisher_ = this->create_publisher<std_msgs::msg::Int32>("/arm_actuator", 10);
+        bucket_publisher_ = this->create_publisher<std_msgs::msg::Int32>("/bucket_actuator", 10);
     }
 
 private:
     void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg) const {
-        // 1. Handle Drive (Twist)
+        // --- 1. Handle Drive (Twist) ---
         auto twist = geometry_msgs::msg::Twist();
-        twist.linear.x = msg->axes[1] * 1.0; 
-        twist.angular.z = msg->axes[3] * -1.0; // Inverting Z is common for natural steering
-        publisher_->publish(twist);
-
-        // 2. Handle Lift Logic
-        auto lift_msg = std_msgs::msg::Int32();
         
+        // Basic deadzone logic (0.05) to prevent drifting
+        double linear_val = msg->axes[1];
+        double angular_val = msg->axes[3];
+
+        twist.linear.x = (std::abs(linear_val) > 0.05) ? linear_val * 1.0 : 0.0;
+        twist.angular.z = (std::abs(angular_val) > 0.05) ? angular_val * -1.0 : 0.0;
+        
+        drive_publisher_->publish(twist);
+
+        // --- 2. Handle Arm Logic ---
+        auto arm_msg = std_msgs::msg::Int32();
+        
+        // Using Buttons 5 (R1) and 4 (L1) as per your original code
         if (msg->buttons[5] == 1) {
-            lift_msg.data = 1;  // Extend
+            arm_msg.data = 1;  // Extend
         } 
         else if (msg->buttons[4] == 1) {
-            lift_msg.data = -1; // Retract
+            arm_msg.data = -1; // Retract
         } 
         else {
-            lift_msg.data = 0;  // Stop
+            arm_msg.data = 0;  // Stop
         }
-        
-        lift_publisher_->publish(lift_msg);
+        arm_publisher_->publish(arm_msg);
+
+        // --- 3. Handle Bucket Logic ---
+        auto bucket_msg = std_msgs::msg::Int32();
+
+        // Added Bucket Logic: Using Buttons 0 and 1 as placeholders
+        // Change these indices once you know which buttons you prefer!
+        if (msg->buttons[0] == 1) {
+            bucket_msg.data = 1;  // Extend/Dump
+        }
+        else if (msg->buttons[1] == 1) {
+            bucket_msg.data = -1; // Retract/Tilt
+        }
+        else {
+            bucket_msg.data = 0;  // Stop
+        }
+        bucket_publisher_->publish(bucket_msg);
     }
 
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
-    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr lift_publisher_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr drive_publisher_;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr arm_publisher_;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr bucket_publisher_;
 };
+
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
--- lunabot_ws/src/lunabot_test_teensy/src/Lin_actuator_test/Lin_actuator_test.ino
+++ lunabot_ws/src/lunabot_test_teensy/src/Lin_actuator_test/Lin_actuator_test.ino
@@ -8,10 +8,7 @@
 #include <std_msgs/msg/int32.h>
 #include <ctype.h>
 
-// --- Hardware Toggles ---
-const bool invert_right_side = false; 
-
-// --- Locale Fix for Teensy/micro-ROS ---
+// --- Locale Fix for Teensy ---
 #ifdef __locale_ctype_ptr
 #undef __locale_ctype_ptr
 #endif
@@ -24,17 +21,26 @@ extern "C" {
 }
 
 // --- Hardware Pins ---
-const int motorLeft1 = 0; 
-const int motorLeft2 = 1;
-const int motorRight1 = 7; 
-const int motorRight2 = 8;
-const int LIFT_PWM_PIN = 19;
-const int LIFT_DIR_PIN = 17;
+const int S1_PIN = 0; // Sabertooth S1 (Left)
+const int S2_PIN = 7; // Sabertooth S2 (Right)
+const int ARM_PWM_PIN = 19;
+const int ARM_DIR_PIN = 17;
+const int BUCKET_PWM_PIN = 18;
+const int BUCKET_DIR_PIN = 20;
 
-// --- PWM Constants (12-bit Resolution) ---
-const int driveNeutral = 307;   // 1500us at 50Hz
-const int driveRange   = 100;   
-const int liftFullPower = 4095; // 100% duty cycle at 12-bit
+// --- PWM Constants (12-bit @ 50Hz) ---
+const int driveNeutral = 307;  // 1500us
+const int driveRange   = 102;  // Full sweep 1ms to 2ms
+const int ACTUATOR_FULL = 4095; 
+
+// --- EMA Smoothing Variables ---
+float left_ema = 0.0;
+float right_ema = 0.0;
+const float alpha = 0.2; // Smoothing factor (Adjust 0.05 to 0.5)
+
+// --- Watchdog ---
+unsigned long last_msg_time = 0;
+const unsigned long TIMEOUT_MS = 500;
 
 // --- micro-ROS Objects ---
 rclc_support_t support;
@@ -44,9 +50,10 @@ rclc_executor_t executor;
 
 rcl_subscription_t drive_sub;
 geometry_msgs__msg__Twist drive_msg;
-
-rcl_subscription_t lift_sub;
-std_msgs__msg__Int32 lift_msg;
+rcl_subscription_t arm_sub;
+std_msgs__msg__Int32 arm_msg;
+rcl_subscription_t bucket_sub;
+std_msgs__msg__Int32 bucket_msg;
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
@@ -58,44 +65,58 @@ void error_loop(){
   }
 }
 
-// --- Drivetrain Callback ---
 void drive_callback(const void * msvin) {
   const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msvin;
-  
-  float left_val  = msg->linear.x - msg->angular.z;
-  float right_val = msg->linear.x + msg->angular.z;
+  last_msg_time = millis();
+
+  float target_left  = msg->linear.x - msg->angular.z;
+  float target_right = msg->linear.x + msg->angular.z;
 
   // Normalization
-  float max_val = max(abs(left_val), abs(right_val));
-  if (max_val > 1.0) {
-    left_val /= max_val;
-    right_val /= max_val;
-  }
+  float max_val = fmaxf(1.0, fmaxf(fabs(target_left), fabs(target_right)));
+  target_left /= max_val;
+  target_right /= max_val;
 
-  if (invert_right_side) right_val = -right_val;
+  // Apply EMA Smoothing
+  left_ema  = (alpha * target_left) + ((1.0 - alpha) * left_ema);
+  right_ema = (alpha * target_right) + ((1.0 - alpha) * right_ema);
 
-  int leftPWM  = driveNeutral + (int)(left_val * driveRange);
-  int rightPWM = driveNeutral + (int)(right_val * driveRange);
+  int leftPWM  = driveNeutral + (int)(left_ema * driveRange);
+  int rightPWM = driveNeutral + (int)(right_ema * driveRange);
   
-  analogWrite(motorLeft1, leftPWM);
-  analogWrite(motorLeft2, leftPWM);
-  analogWrite(motorRight1, rightPWM);
-  analogWrite(motorRight2, rightPWM);
+  analogWrite(S1_PIN, leftPWM);
+  analogWrite(S2_PIN, rightPWM);
 }
 
-// --- Lift Callback ---
-void lift_callback(const void * msvin) {
+void arm_callback(const void * msvin) {
   const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msvin;
   int command = msg->data;
+  last_msg_time = millis();
 
   if (command == 1) {
-    digitalWrite(LIFT_DIR_PIN, HIGH);
-    analogWrite(LIFT_PWM_PIN, liftFullPower);
+    digitalWrite(ARM_DIR_PIN, HIGH);
+    analogWrite(ARM_PWM_PIN, ACTUATOR_FULL);
   } else if (command == -1) {
-    digitalWrite(LIFT_DIR_PIN, LOW);
-    analogWrite(LIFT_PWM_PIN, liftFullPower);
+    digitalWrite(ARM_DIR_PIN, LOW);
+    analogWrite(ARM_PWM_PIN, ACTUATOR_FULL);
   } else {
-    analogWrite(LIFT_PWM_PIN, 0);
+    analogWrite(ARM_PWM_PIN, 0);
+  }
+}
+
+void bucket_callback(const void * msvin) {
+  const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msvin;
+  int command = msg->data;
+  last_msg_time = millis();
+
+  if (command == 1) {
+    digitalWrite(BUCKET_DIR_PIN, HIGH);
+    analogWrite(BUCKET_PWM_PIN, ACTUATOR_FULL);
+  } else if (command == -1) {
+    digitalWrite(BUCKET_DIR_PIN, LOW);
+    analogWrite(BUCKET_PWM_PIN, ACTUATOR_FULL);
+  } else {
+    analogWrite(BUCKET_PWM_PIN, 0);
   }
 }
 
@@ -103,45 +124,57 @@ void setup() {
   set_microros_transports();
   
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(motorLeft1, OUTPUT); pinMode(motorLeft2, OUTPUT);
-  pinMode(motorRight1, OUTPUT); pinMode(motorRight2, OUTPUT);
-  pinMode(LIFT_PWM_PIN, OUTPUT); pinMode(LIFT_DIR_PIN, OUTPUT);
+  pinMode(S1_PIN, OUTPUT); 
+  pinMode(S2_PIN, OUTPUT);
+  pinMode(ARM_PWM_PIN, OUTPUT); 
+  pinMode(ARM_DIR_PIN, OUTPUT);
+  pinMode(BUCKET_PWM_PIN, OUTPUT); 
+  pinMode(BUCKET_DIR_PIN, OUTPUT);
 
   // Frequency Setup
-  // NOTE: Pins 0, 1, 7, 8 are on different timers than Pin 19 on Teensy 4.1
-  analogWriteFrequency(motorLeft1, 50);
-  analogWriteFrequency(motorRight1, 50);
-  analogWriteFrequency(LIFT_PWM_PIN, 5000); 
-  
+  analogWriteFrequency(S1_PIN, 50); // R/C Pulse frequency
+  analogWriteFrequency(S2_PIN, 50);
+  analogWriteFrequency(ARM_PWM_PIN, 5000); 
+  analogWriteFrequency(BUCKET_PWM_PIN, 5000); 
+
   analogWriteResolution(12);
   
-  // Initial States
-  analogWrite(motorLeft1, driveNeutral);
-  analogWrite(motorLeft2, driveNeutral);
-  analogWrite(motorRight1, driveNeutral);
-  analogWrite(motorRight2, driveNeutral);
-  analogWrite(LIFT_PWM_PIN, 0);
+  // Initial States (Stop)
+  analogWrite(S1_PIN, driveNeutral);
+  analogWrite(S2_PIN, driveNeutral);
+  analogWrite(ARM_PWM_PIN, 0);
+  analogWrite(BUCKET_PWM_PIN, 0);
 
-  // Wait for Agent Handshake (Prevents the crash/blink)
+  // Agent Handshake
   while (RMW_RET_OK != rmw_uros_ping_agent(100, 1)) {
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    delay(500); 
+    delay(250); 
   }
 
   allocator = rcl_get_default_allocator();
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
   RCCHECK(rclc_node_init_default(&node, "teensy_robot_node", "", &support));
 
-  // Subscriptions
   RCCHECK(rclc_subscription_init_default(&drive_sub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist), "cmd_vel"));
-  RCCHECK(rclc_subscription_init_default(&lift_sub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "lift_actuator"));
+  RCCHECK(rclc_subscription_init_default(&arm_sub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "arm_actuator"));
+  RCCHECK(rclc_subscription_init_default(&bucket_sub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "bucket_actuator"));
 
-  // Executor (Handle count MUST be 2)
-  RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+  RCCHECK(rclc_executor_init(&executor, &support.context, 3, &allocator));
   RCCHECK(rclc_executor_add_subscription(&executor, &drive_sub, &drive_msg, &drive_callback, ON_NEW_DATA));
-  RCCHECK(rclc_executor_add_subscription(&executor, &lift_sub, &lift_msg, &lift_callback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_subscription(&executor, &arm_sub, &arm_msg, &arm_callback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_subscription(&executor, &bucket_sub, &bucket_msg, &bucket_callback, ON_NEW_DATA));
 }
 
 void loop() {
+  // Check for communication timeout
+  if (millis() - last_msg_time > TIMEOUT_MS) {
+    left_ema = 0;
+    right_ema = 0;
+    analogWrite(S1_PIN, driveNeutral);
+    analogWrite(S2_PIN, driveNeutral);
+    analogWrite(ARM_PWM_PIN, 0);
+    analogWrite(BUCKET_PWM_PIN, 0);
+  }
+
   RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)));
 }
